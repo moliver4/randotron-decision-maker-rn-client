33c842446d322a730e41ea9bc4d3c1f6d5f5b0ef
moliver4 <tifftang22@gmail.com>
2020-02-11 10:41:54 -0800

    imported additional color as new accent

--- components/DecisionCard.js
+++ components/DecisionCard.js
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
 import { View, Text, Dimensions, StyleSheet } from 'react-native';
+import Colors from '../constants/Colors'
 
 let winSize = Dimensions.get('window');
 
 const DecisionCard = ( { choice } ) => {
 
   return (
- 
         <Text style={styles.answer}> 
           {choice.title}
         </Text>
@@ -18,6 +18,7 @@ const styles = StyleSheet.create({
 
     answer: {
       textAlign: 'center',
+      color: Colors.extra,
       fontSize: 80/winSize.scale
     }
 })
--- constants/Colors.js
+++ constants/Colors.js
@@ -1,5 +1,6 @@
 export default {
     primary: '#5E656A',
     accent: '#CFF8F0',
+    additional: '#fc9208',
     extra: '#37D6A1'
 };
